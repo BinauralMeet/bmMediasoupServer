c71c359ce01702ba425f0bfa286de3e9780c0ddb
Shoichi Hasegawa <hasevr@gmail.com>
2022-06-19 00:24:30 +0900

    Feat: producer/consumer

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import websocket from 'ws'
 import https from 'https'
 import fs from 'fs'
 import debugModule from 'debug'
-import {ConnectTransportMessage, Message, MessageType, RoomMessage, TransportCreatedMessage} from './MediaMessages'
+import {MSConnectTransportMessage, MSMessage, MSMessageType, MSRoomMessage, MSCreateTransportReply,
+  MSPeerMessage, MSProduceTransportReply, MSRemoteProducer, MSRemotePeer, MSRemoteUpdateMessage, MSCloseTransportMessage} from './MediaMessages'
+import { exit } from 'process'
 
 const log = debugModule('bmMsM');
 const warn = debugModule('bmMsM:WARN');
@@ -11,8 +13,9 @@ const config = require('../config');
 
 /*
     main server only for signaling
-      knows endpoints, producers and consumers
-    
+      knows peers=endpoints, rooms, producers and consumers
+      a peer can join to only one room.
+
     each media server has 1 worker and router
     media server 1 has producer1 and consumers
     media server 2 has producer2 and consumers
@@ -27,27 +30,39 @@ interface Worker{
 }
 const workers = new Map<string, Worker>()
 
-interface Transport{
-  id: string
-  ws: websocket.WebSocket
-}
-const transports = new Map<string, Transport>()
-
 function getVacantWorker(){
-  return Array.from(workers.values()).reduce((prev, cur)=> prev.stat.load < prev.stat.load ? prev : cur)
+  if (workers.size){
+    return Array.from(workers.values()).reduce((prev, cur)=> prev.stat.load < prev.stat.load ? prev : cur)
+  }
+  return undefined
 }
 
-interface Peer{
-  id: string
+interface Peer extends MSRemotePeer{
   ws: websocket.WebSocket
-  producers: string[]
+  room?: Room
+  worker?: Worker
+  transports:string[]
 }
+function toMSRemotePeer(peer: Peer):MSRemotePeer{
+  const {ws, room, worker, ...ms} = peer
+  return ms
+}
+
 const peers = new Map<string, Peer>()
 interface Room{
   id: string
-  peers: Set<string>
+  peers: Set<Peer>
 }
 const rooms = new Map<string, Room>()
+
+function send<MSM extends MSMessage>(msg: MSM, ws: websocket.WebSocket){
+  ws.send(JSON.stringify(msg))
+}
+function sendRoom<MSM extends MSMessage>(msg: MSM, room:Room){
+  for(const peer of room.peers.values()){
+    peer.ws.send(JSON.stringify(msg))
+  }
+}
 
 function makeUniqueId(id:string, map: Map<string, any>){
   if (!map.has(id)){
@@ -61,73 +76,156 @@ function makeUniqueId(id:string, map: Map<string, any>){
   }
 }
 
-const handlers = new Map<MessageType, (ws:websocket.WebSocket, base:Message)=>void>()
-handlers.set('connect',(ws, base)=>{
-  const unique = makeUniqueId(base.peer, peers)
-  base.peer = unique
-  ws.send(JSON.stringify(base))
-  peers.set(unique, {id:unique, ws, producers:[]})
+function getPeerAndWorker(id: string){
+  const peer = peers.get(id)
+  if (!peer) {
+    console.error(`Peer ${id} not found.`)
+    exit()
+  }
+  if (!peer.worker) peer.worker = getVacantWorker()
+  return peer
+}
+const handlersForPeer = new Map<MSMessageType, (base:MSMessage, ws:websocket.WebSocket)=>void>()
+const handlersForWorker = new Map<MSMessageType, (base:MSMessage, ws:websocket.WebSocket)=>void>()
+
+function getPeer(id: string):Peer{
+  const peer = peers.get(id)
+  if (!peer){
+    console.error(`peer ${id} not found.`)
+    return {peer:'', producers:[], transports:[], ws:new websocket.WebSocket('')}
+  }
+  return peer
+}
+function deletePeer(id: string){
+  const peer = getPeer(id)
+  peer.room?.peers.delete(peer)
+  checkDeleteRoom(peer.room)
+}
+function checkDeleteRoom(room?: Room){
+  if (room && room.peers.size === 0){
+    rooms.delete(room.id)
+  }
+}
+
+handlersForPeer.set('connect',(base, ws)=>{
+  const msg = base as MSPeerMessage
+  const unique = makeUniqueId(msg.peer, peers)
+  msg.peer = unique
+  send(msg, ws)
+  peers.set(unique, {peer:unique, ws, producers:[], transports:[]})
   console.log(`${unique} connected: ${JSON.stringify(Array.from(peers.keys()))}`)
 })
-handlers.set('join',(ws, base)=>{
-  const join = base as RoomMessage
-  const room = rooms.get(join.room)
-  console.log(`${join.peer} joined to room ${join.room}`)
+handlersForPeer.set('join',(base, ws)=>{
+  const msg = base as MSPeerMessage
+  const peer = getPeer(msg.peer)
+  const join = base as MSRoomMessage
+  let room = rooms.get(join.room)
+  console.log(`${peer.peer} joined to room ${join.room}`)
   if (room) {
-    room.peers.add(join.peer)
+    room.peers.add(peer)
   }else{
-    rooms.set(join.room, {id:join.room, peers:new Set<string>([join.peer])})
+    room = {id:join.room, peers:new Set<Peer>([peer])}
+    rooms.set(room.id, room)
     console.log(`room ${join.room} created: ${JSON.stringify(Array.from(rooms.keys()))}`)
   }
-})
-handlers.set('leave',(ws, base)=>{
-  const leave = base as RoomMessage
-  const room = rooms.get(leave.room)
-  if (room) {
-    room.peers.delete(leave.peer)
+  peer.room = room
+  //  notify the room's remotes
+  const remoteUpdateMsg:MSRemoteUpdateMessage = {
+    type:'remoteUpdate',
+    remotes: Array.from(peer.room.peers).map(peer => toMSRemotePeer(peer))
   }
+  send(remoteUpdateMsg, ws)
 })
-handlers.set('addWorker',(ws, base)=>{
-  const unique = makeUniqueId(base.peer, workers)
-  base.peer = unique
-  ws.send(JSON.stringify(base))
-  const {type, peer, ...msg_minus} = base
-  workers.set(base.peer, {...msg_minus, id:base.peer, ws, stat:{load:0}})
-  console.log(`addWorker ${base.peer}`)
+handlersForPeer.set('leave',(base)=>{
+  const msg = base as MSPeerMessage
+  deletePeer(msg.peer)
 })
-handlers.set('deleteWorker',(ws, base)=>{
-  workers.delete(base.peer)
+handlersForPeer.set('addWorker',(base, ws)=>{
+  const msg = base as MSPeerMessage
+  const unique = makeUniqueId(msg.peer, workers)
+  msg.peer = unique
+  send(msg, ws)
+  const {type, peer, ...msg_minus} = msg
+  workers.set(msg.peer, {...msg_minus, id:msg.peer, ws, stat:{load:0}})
+  ws.removeEventListener('message', onWsMessagePeer)
+  ws.addEventListener('message', onWsMessageWorker)
+  console.log(`addWorker ${msg.peer}`)
 })
-handlers.set('createTransport', (ws, base) => {
-  const worker = getVacantWorker()
-  worker.ws.send(JSON.stringify(base))
+handlersForWorker.set('deleteWorker',(base, ws)=>{
+  const msg = base as MSPeerMessage
+  workers.delete(msg.peer)
 })
-handlers.set('transportCreated', (ws, base) => {
-  const msg = base as TransportCreatedMessage
+
+function relayPeerToWorker(base: MSMessage){
+  const msg = base as MSPeerMessage
+  const peer = getPeerAndWorker(msg.peer)
+  if (peer.worker){
+    send(msg, peer.worker.ws)
+  }
+}
+function relayWorkerToPeer(base: MSMessage){
+  const msg = base as MSPeerMessage
   const peer = peers.get(msg.peer)
   if (peer){
-    peer.ws.send(JSON.stringify(msg))
+    send(msg, peer.ws)
   }
-})
-handlers.set('connectTransport', (ws, base) => {
-  const msg = base as ConnectTransportMessage
-  const wws = transports.get(msg.transportId)?.ws
-  if (wws){ wws.send(JSON.stringify(msg)) }
+}
+function setRelayHandlers(mt: MSMessageType){
+  handlersForPeer.set(mt, relayPeerToWorker)
+  handlersForWorker.set(mt, relayWorkerToPeer)
+}
+setRelayHandlers('rtpCapabilities')
+handlersForPeer.set('createTransport', relayPeerToWorker)
+handlersForWorker.set('createTransport', (base, ws)=>{
+  const msg = base as MSCreateTransportReply
+  const peer = getPeer(msg.peer)
+  if (msg.transport){ peer.transports.push(msg.transport) }
+  send(base, peer.ws)
 })
 
-
-function onWsConnection(ws: websocket.WebSocket){
-  console.log(`onConnection() `)
-  ws.on('message', messageData => {
-    console.log(`onMessage(${messageData.toString()})`)
-    const base = JSON.parse(messageData.toString()) as Message
-    const handler = handlers.get(base.type)
-    if (handler){
-      handler(ws, base)
-    }else{
-      console.log(`unhandle message type ${base.type} received from ${base.peer}`)
+setRelayHandlers('connectTransport')
+handlersForPeer.set('produceTransport', relayPeerToWorker)
+handlersForWorker.set('produceTransport', (base, ws)=>{
+  const msg = base as MSProduceTransportReply
+  const peer = getPeer(msg.peer)
+  if (msg.producer){
+    if (peer.producers.find(p => p.role === msg.role && p.kind === msg.kind)){
+      console.error(`A producer for the same role ${msg.role} and kind ${msg.kind} already exists.`)
     }
-  })
+    peer.producers.push({id:msg.producer, kind: msg.kind, role: msg.role})
+  }
+  send(base, peer.ws)
+  const remoteUpdateMsg:MSRemoteUpdateMessage = {
+    type:'remoteUpdate',
+    remotes: [toMSRemotePeer(peer)]
+  }
+  sendRoom(remoteUpdateMsg, peer.room!)
+})
+setRelayHandlers('consumeTransport')
+
+function onWsMessagePeer(messageData: websocket.MessageEvent){
+  const ws = messageData.target
+  console.log(`onMessagePeer(${messageData.data.toString()})`)
+  const base = JSON.parse(messageData.data.toString()) as MSMessage
+  const handler = handlersForPeer.get(base.type)
+  if (handler){
+    handler(base, ws)
+  }else{
+    const msg = base as MSPeerMessage
+    console.log(`Unhandle peer message ${msg.type} received from ${msg.peer}`)
+  }
+}
+function onWsMessageWorker(messageData: websocket.MessageEvent){
+  const ws = messageData.target
+  console.log(`onMessageWorker(${messageData.data.toString()})`)
+  const base = JSON.parse(messageData.data.toString()) as MSMessage
+  const handler = handlersForWorker.get(base.type)
+  if (handler){
+    handler(base, ws)
+  }else{
+    const msg = base as MSPeerMessage
+    console.log(`Unhandle worker message ${msg.type} received from ${msg.peer}`)
+  }
 }
 
 async function main() {
@@ -138,14 +236,28 @@ async function main() {
       cert: fs.readFileSync(config.sslCrt),
       key: fs.readFileSync(config.sslKey),
     };
-    const httpsServer = https.createServer(tls);      
+    const httpsServer = https.createServer(tls);
     httpsServer.on('error', (e) => {
       console.error('https server error,', e.message);
     });
 
     const wss = new websocket.Server({server: httpsServer})
     wss.on('connection', ws => {
-      onWsConnection(ws)
+      console.log(`onConnection() `)
+      ws.addEventListener('message', onWsMessagePeer)
+      ws.addEventListener('close', (ev) =>{
+        const peer = Array.from(peers.values()).find(p => p.ws === ws)
+        if (peer){
+          peer.transports.forEach(transport => {
+            const msg: MSCloseTransportMessage= {
+              type: 'closeTransport',
+              transport,
+            }
+            send(msg, peer.worker!.ws)
+          })
+          peers.delete(peer.peer)
+        }
+      })
     })
 
     await new Promise<void>((resolve) => {
--- src/media.ts
+++ src/media.ts
@@ -1,9 +1,8 @@
 import websocket from 'ws'
 import * as mediasoup from 'mediasoup'
-import fs from 'fs'
 import debugModule from 'debug'
-import { argv } from 'process'
-import {Message, MessageType} from './MediaMessages'
+import {MSCreateTransportMessage, MSMessage, MSMessageType, MSCreateTransportReply, MSRTPCapabilitiesReply,
+   MSConnectTransportMessage, MSConnectTransportReply, MSProduceTransportReply, MSProduceTransportMessage, MSPeerMessage, MSConsumeTransportMessage, MSConsumeTransportReply} from './MediaMessages'
 
 import { networkInterfaces } from "os";
 function getIpAddress() {
@@ -20,7 +19,7 @@ const config = require('../config');
 /*
     main server only for signaling
       knows endpoints, producers and consumers
-    
+
     each media server has 1 worker and router
     media server 1 has producer1 and consumers
     media server 2 has producer2 and consumers
@@ -60,18 +59,173 @@ async function startMediasoup() {
   return { worker, router, audioLevelObserver };
 }
 
-const handlers = new Map<MessageType, (ws:websocket.WebSocket, base:Message)=>void>()
-handlers.set('addWorker',(ws, base)=>{
-    workerId = base.peer
-    console.log(`workerId: ${workerId}`)
-})
-  
 
 let workerId = ''
+const transports = new Map<string, mediasoup.types.Transport>()
+const producers = new Map<string, mediasoup.types.Producer>()
+const handlers = new Map<MSMessageType, (base:MSMessage, ws:websocket.WebSocket)=>void>()
+
+function closeProducer(producer:mediasoup.types.Producer) {
+  console.log('closing producer', producer.id, producer.appData);
+  try {
+    producer.close()
+    // remove this producer from our list
+    producers.delete(producer.id)
+  } catch (e) {
+    err(e);
+  }
+}
+function closeConsumer(consumer: mediasoup.types.Consumer) {
+  console.log('closing consumer', consumer.id, consumer.appData);
+  consumer.close();
+}
+
+function send(base: MSMessage, ws: websocket.WebSocket){
+  ws.send(JSON.stringify(base))
+}
+
 async function main() {
   // start mediasoup
   console.log('starting mediasoup')
-  const {worker, router, audioLevelObserver} = await startMediasoup()  
+  const {worker, router, audioLevelObserver} = await startMediasoup()
+
+  handlers.set('addWorker',(base)=>{
+    const msg = base as MSPeerMessage
+    workerId = msg.peer
+    console.log(`workerId: ${workerId}`)
+  })
+
+  handlers.set('rtpCapabilities',(base, ws)=>{
+    const msg = base as MSPeerMessage
+    const sendMsg:MSRTPCapabilitiesReply = {
+      ...msg,
+      rtpCapabilities: router.rtpCapabilities
+    }
+    send(sendMsg, ws)
+  });
+
+  handlers.set('createTransport',(base, ws)=>{
+    const msg = base as MSCreateTransportMessage
+    const {
+      listenIps,
+      initialAvailableOutgoingBitrate
+    } = config.mediasoup.webRtcTransport
+    router.createWebRtcTransport({
+      listenIps: listenIps,
+      enableUdp: true,
+      enableTcp: true,
+      preferUdp: true,
+      initialAvailableOutgoingBitrate: initialAvailableOutgoingBitrate,
+      appData: {peer: msg.peer, dir: msg.dir}
+    }).then(transport=>{
+      const sendMsg:MSCreateTransportReply = {
+        type :'createTransport',
+        peer: msg.peer,
+        sn: msg.sn,
+        transport: transport.id,
+        iceCandidates: transport.iceCandidates,
+        iceParameters: transport.iceParameters,
+        dtlsParameters: transport.dtlsParameters,
+        dir: msg.dir,
+      }
+      transports.set(transport.id, transport)
+      send(sendMsg, ws)
+    });
+  })
+
+  handlers.set('connectTransport', (base, ws) => {
+    const msg = base as MSPeerMessage
+    const sendMsg:MSConnectTransportReply = {
+      type: 'connectTransport',
+      peer: msg.peer,
+      sn: msg.sn,
+      error: ''
+    }
+    try {
+      const msg = base as MSConnectTransportMessage
+      const transport = transports.get(msg.transport)
+      if (!transport) {
+        console.error(`connect-transport: server-side transport ${msg.transport} not found`)
+        sendMsg.error = `server-side transport ${msg.transport} not found`
+        send(sendMsg, ws)
+      }else{
+        transport.connect({dtlsParameters: msg.dtlsParameters}).then(()=>{
+          send(sendMsg, ws)
+        })
+      }
+    } catch (e) {
+      console.error('error in /signaling/connect-transport', e);
+      sendMsg.error = `${e}`
+      send(sendMsg, ws)
+    }
+  })
+
+  handlers.set('produceTransport', (base) => {
+    const msg = base as MSProduceTransportMessage
+    const {rtpParameters, paused, ...msg_} = msg
+    const sendMsg:MSProduceTransportReply = msg_
+    const transport = transports.get(msg.transport)
+    if (!transport) {
+      console.error(`produce-transport: server-side transport ${msg.transport} not found`)
+      sendMsg.error = `server-side transport ${msg.transport} not found`
+      send(sendMsg, ws)
+    }else{
+      transport.produce({
+        kind:msg.kind,
+        rtpParameters: msg.rtpParameters,
+        paused:msg.paused,
+        appData: { peer:msg.peer, transportId: transport.id}
+      }).then((producer)=>{
+        producer.on('transportclose', () => {
+          console.log('producer\'s transport closed', producer.id);
+          closeProducer(producer);
+        });
+        // monitor audio level of this producer. we call addProducer() here,
+        // but we don't ever need to call removeProducer() because the core
+        // AudioLevelObserver code automatically removes closed producers
+        if (producer.kind === 'audio') {
+          audioLevelObserver.addProducer({ producerId: producer.id });
+        }
+
+        producers.set(producer.id, producer)
+        sendMsg.producer = producer.id
+        send(sendMsg, ws)
+      })
+    }
+  })
+
+  handlers.set('consumeTransport', (base) => {
+    const msg = base as MSConsumeTransportMessage
+    const {rtpCapabilities, ...msg_} = msg
+    const sendMsg:MSConsumeTransportReply = {...msg_}
+    const transport = transports.get(msg.transport)
+    if (!transport) {
+      console.error(`consume-transport: server-side transport ${msg.transport} not found`)
+      sendMsg.error = `server-side transport ${msg.transport} not found`
+      send(sendMsg, ws)
+    }else{
+      transport.consume({
+        producerId: msg.producer,
+        rtpCapabilities: msg.rtpCapabilities,
+        paused:true,
+        appData: { peer:msg.peer, transportId: transport.id}
+      }).then((consumer)=>{
+        consumer.on('transportclose', () => {
+          log(`consumer's transport closed`, consumer.id)
+          closeConsumer(consumer)
+        })
+          consumer.on('producerclose', () => {
+            log(`consumer's producer closed`, consumer.id);
+            closeConsumer(consumer)
+        })
+        sendMsg.consumer = consumer.id
+        sendMsg.rtpParameters = consumer.rtpParameters
+        sendMsg.kind = consumer.kind
+        send(sendMsg, ws)
+      })
+    }
+  })
+
 
   // start https server, falling back to http if https fails
   console.log('connecting to main server');
@@ -79,17 +233,18 @@ async function main() {
     ws.onopen = (ev) => {
         let ip = getIpAddress()
         if (!ip) ip = 'localhost'
-        const msg:Message = {
+        const msg:MSPeerMessage = {
             type:'addWorker',
             peer:`${ip}_${worker.pid}`
         }
-        ws.send(JSON.stringify(msg))
+        console.log(`send ${JSON.stringify(msg)}`)
+        send(msg, ws)
     }
     ws.onmessage = (ev)=>{
-        const base = JSON.parse(ev.data.toString()) as Message
+        const base = JSON.parse(ev.data.toString()) as MSMessage
         const handler = handlers.get(base.type)
         if (handler){
-            handler(ws, base)
+            handler(base, ws)
         }
     }
 }
