938e2a4a0a2d2b7fb3d362cb7f6d2733fa03495f
Shoichi Hasegawa <hasevr@gmail.com>
2022-06-28 10:41:51 +0900

    Feat: iceServers given from media.ts.

--- config.js
+++ config.js
@@ -1,90 +1,89 @@
 module.exports = {
-    mainServer: "wss://localhost:3100",
+  //----------------------------------------------------------
+  //  for main server
+  //  main server's http server ip, port, and peer timeout constant
+  httpIp: "0.0.0.0",  //  ip to listen
+  httpPort: 3100,      //  port to listen
+  //httpPort: 443,      //  port to listen
+  httpPeerStale: 15000,
+  sslCrt: './certs/localhost.pem',
+  sslKey: './certs/localhost-key.pem',
 
-    // http server ip, port, and peer timeout constant
-    //
-    httpIp: "0.0.0.0",
-    httpPort: 3100,
-    httpPeerStale: 15000,
-  
-    // ssl certs. we'll start as http instead of https if we don't have
-    // these
-    //sslCrt: "local.crt",
-    //sslKey: "local.key",
-    sslCrt: './certs/localhost.pem',
-    sslKey: './certs/localhost-key.pem',
-  
-    mediasoup: {
-      worker: {
-        rtcMinPort: 40000,
-        rtcMaxPort: 49999,
-        logLevel: "debug",
-        logTags: [
-          "info",
-          "ice",
-          "dtls",
-          "rtp",
-          "srtp",
-          "rtcp",
-          // 'rtx',
-          // 'bwe',
-          // 'score',
-          // 'simulcast',
-          // 'svc'
-        ],
-      },
-      router: {
-        mediaCodecs: [
-          {
-            kind: "audio",
-            mimeType: "audio/opus",
-            clockRate: 48000,
-            channels: 2,
-          },
-          {
-            kind: "video",
-            mimeType: "video/VP8",
-            clockRate: 90000,
-            parameters: {
-              //                'x-google-start-bitrate': 1000
-            },
-          },
-          {
-            kind: "video",
-            mimeType: "video/h264",
-            clockRate: 90000,
-            parameters: {
-              "packetization-mode": 1,
-              "profile-level-id": "4d0032",
-              "level-asymmetry-allowed": 1,
-              //						  'x-google-start-bitrate'  : 1000
-            },
-          },
-          {
-            kind: "video",
-            mimeType: "video/h264",
-            clockRate: 90000,
-            parameters: {
-              "packetization-mode": 1,
-              "profile-level-id": "42e01f",
-              "level-asymmetry-allowed": 1,
-              //						  'x-google-start-bitrate'  : 1000
-            },
-          },
-        ],
-      },
-  
-      // rtp listenIps are the most important thing, below. you'll need
-      // to set these appropriately for your network for the demo to
-      // run anywhere but on localhost
-      webRtcTransport: {
-        listenIps: [
-          { ip: "127.0.0.1", announcedIp: null },
-          // { ip: "192.168.42.68", announcedIp: null },
-          // { ip: '10.10.23.101', announcedIp: null },
-        ],
-        initialAvailableOutgoingBitrate: 800000,
-      },
+  //----------------------------------------------------------
+  //  for media server
+  //  url to main server
+  mainServer: "wss://localhost:3100", //  url to the main server
+  //mainServer: "wss://msmain.titech.binaural.me", //  url to the main server
+  mediasoup: {
+    worker: {
+      rtcMinPort: 40000,
+      rtcMaxPort: 49999,
+      logLevel: "debug",
+      logTags: [
+        "info",
+        "ice",
+        "dtls",
+        "rtp",
+        "srtp",
+        "rtcp",
+        // 'rtx',
+        // 'bwe',
+        // 'score',
+        // 'simulcast',
+        // 'svc'
+      ],
     },
-  };
-  
+    router: {
+      mediaCodecs: [
+        {
+          kind: "audio",
+          mimeType: "audio/opus",
+          clockRate: 48000,
+          channels: 2,
+        },
+        {
+          kind: "video",
+          mimeType: "video/VP8",
+          clockRate: 90000,
+          parameters: {
+            //                'x-google-start-bitrate': 1000
+          },
+        },
+        {
+          kind: "video",
+          mimeType: "video/h264",
+          clockRate: 90000,
+          parameters: {
+            "packetization-mode": 1,
+            "profile-level-id": "4d0032",
+            "level-asymmetry-allowed": 1,
+            //						  'x-google-start-bitrate'  : 1000
+          },
+        },
+        {
+          kind: "video",
+          mimeType: "video/h264",
+          clockRate: 90000,
+          parameters: {
+            "packetization-mode": 1,
+            "profile-level-id": "42e01f",
+            "level-asymmetry-allowed": 1,
+            //						  'x-google-start-bitrate'  : 1000
+          },
+        },
+      ],
+    },
+
+    // rtp listenIps are the most important thing, below. you'll need
+    // to set these appropriately for your network for the demo to
+    // run anywhere but on localhost
+    webRtcTransport: {
+      listenIps: [
+        //  no entry = auto find = try to find host's ip or use 127.0.0.1
+        //{ ip: "127.0.0.1", announcedIp: null },
+        // { ip: '10.10.23.101', announcedIp: null },
+      ],
+      initialAvailableOutgoingBitrate: 800000,
+    },
+  },
+};
--- src/media.ts
+++ src/media.ts
@@ -3,10 +3,35 @@ import * as mediasoup from 'mediasoup'
 import debugModule from 'debug'
 import {MSCreateTransportMessage, MSMessage, MSMessageType, MSCreateTransportReply, MSRTPCapabilitiesReply,
    MSConnectTransportMessage, MSConnectTransportReply, MSProduceTransportReply, MSProduceTransportMessage, MSPeerMessage, MSConsumeTransportMessage, MSConsumeTransportReply, MSResumeConsumerMessage, MSResumeConsumerReply, MSCloseProducerMessage, MSCloseProducerReply, MSWorkerUpdateMessage} from './MediaMessages'
-import { networkInterfaces } from "os";
+import * as os from 'os'
+import * as dns from 'dns'
 
+
+interface FqdnAndIp{
+  fqdn: string, ip: string
+}
+
+function getFqdnAndIp(){
+  const promise = new Promise<FqdnAndIp>((resolve, reject)=>{
+    var h = os.hostname()
+    dns.lookup(h, { hints: dns.ADDRCONFIG }, function(err, ip) {
+      //console.log('IP: ' + ip)
+      dns.lookupService(ip, 0, function (err, hostname, service) {
+        if (err) {
+          console.log(err)
+          reject()
+          return
+        }
+        //console.log('FQDN: ' + hostname)
+        //console.log('Service: ' + service)
+        resolve({fqdn:hostname, ip})
+      })
+    })
+  })
+  return promise
+}
 function getIpAddress() {
-  const nets = networkInterfaces();
+  const nets = os.networkInterfaces();
   const net = nets["en0"]?.find((v) => v.family == "IPv4");
   return !!net ? net.address : null;
 }
@@ -111,223 +136,252 @@ function send(base: MSMessage, ws: websocket.WebSocket){
   ws.send(JSON.stringify(base))
 }
 
+let hostinfo:FqdnAndIp
+//  get host info
+getFqdnAndIp().then(info => {
+  hostinfo = info
+  start()
+})
+
 // start mediasoup
-console.log('starting mediasoup')
-startMediasoup().then(({worker, router}) => {
-  //  set message handlers
-  handlers.set('workerAdd',(base)=>{
-    const msg = base as MSPeerMessage
-    workerId = msg.peer
-    console.log(`workerId: ${workerId}`)
-  })
+function start(){
+  console.log('starting mediasoup')
+  startMediasoup().then(({worker, router}) => {
+    //  set message handlers
+    handlers.set('workerAdd',(base)=>{
+      const msg = base as MSPeerMessage
+      workerId = msg.peer
+      console.log(`workerId: ${workerId}`)
+    })
 
-  handlers.set('rtpCapabilities',(base, ws)=>{
-    const msg = base as MSPeerMessage
-    const sendMsg:MSRTPCapabilitiesReply = {
-      ...msg,
-      rtpCapabilities: router.rtpCapabilities
-    }
-    send(sendMsg, ws)
-  });
-
-  handlers.set('createTransport',(base, ws)=>{
-    const msg = base as MSCreateTransportMessage
-    const {
-      listenIps,
-      initialAvailableOutgoingBitrate
-    } = config.mediasoup.webRtcTransport
-    router.createWebRtcTransport({
-      listenIps: listenIps,
-      enableUdp: true,
-      enableTcp: true,
-      preferUdp: true,
-      initialAvailableOutgoingBitrate: initialAvailableOutgoingBitrate,
-      appData: {peer: msg.peer, dir: msg.dir}
-    }).then(transport=>{
-      const sendMsg:MSCreateTransportReply = {
-        type :'createTransport',
-        peer: msg.peer,
-        sn: msg.sn,
-        transport: transport.id,
-        iceCandidates: transport.iceCandidates,
-        iceParameters: transport.iceParameters,
-        dtlsParameters: transport.dtlsParameters,
-        dir: msg.dir,
+    handlers.set('rtpCapabilities',(base, ws)=>{
+      const msg = base as MSPeerMessage
+      const sendMsg:MSRTPCapabilitiesReply = {
+        ...msg,
+        rtpCapabilities: router.rtpCapabilities
       }
-      transports.set(transport.id, transport)
       send(sendMsg, ws)
     });
-  })
 
-  handlers.set('connectTransport', (base, ws) => {
-    const msg = base as MSPeerMessage
-    const sendMsg:MSConnectTransportReply = {
-      type: 'connectTransport',
-      peer: msg.peer,
-      sn: msg.sn,
-      error: ''
-    }
-    try {
-      const msg = base as MSConnectTransportMessage
+    handlers.set('createTransport',(base, ws)=>{
+      const msg = base as MSCreateTransportMessage
+      const {
+        listenIps,
+        initialAvailableOutgoingBitrate
+      } = config.mediasoup.webRtcTransport
+      if (listenIps.length === 0){
+        let ip = hostinfo.ip
+        if (!ip) ip = '127.0.0.1'
+        listenIps.push({ ip, announcedIp: null })
+      }
+
+      router.createWebRtcTransport({
+        listenIps: listenIps,
+        enableUdp: true,
+        enableTcp: true,
+        preferUdp: true,
+        initialAvailableOutgoingBitrate: initialAvailableOutgoingBitrate,
+        appData: {peer: msg.peer, dir: msg.dir}
+      }).then(transport=>{
+        const sendMsg:MSCreateTransportReply = {
+          type :'createTransport',
+          peer: msg.peer,
+          sn: msg.sn,
+          transport: transport.id,
+          iceCandidates: transport.iceCandidates,
+          iceParameters: transport.iceParameters,
+          dtlsParameters: transport.dtlsParameters,
+          dir: msg.dir,
+          iceServers: [
+            {
+              urls: `turn:${hostinfo.fqdn}`,
+              username: 'binauralmeet',
+              credential: 'binauralmeet_mediasoup_server',
+              credentialType:'password'
+            },
+            {
+              urls: `turns:${hostinfo.fqdn}:443`,
+              username: 'binauralmeet',
+              credential: 'binauralmeet_mediasoup_server',
+              credentialType:'password'
+            },
+          ]
+        }
+        transports.set(transport.id, transport)
+        send(sendMsg, ws)
+      });
+    })
+
+    handlers.set('connectTransport', (base, ws) => {
+      const msg = base as MSPeerMessage
+      const sendMsg:MSConnectTransportReply = {
+        type: 'connectTransport',
+        peer: msg.peer,
+        sn: msg.sn,
+        error: ''
+      }
+      try {
+        const msg = base as MSConnectTransportMessage
+        const transport = transports.get(msg.transport)
+        if (!transport) {
+          console.error(`connect-transport: server-side transport ${msg.transport} not found`)
+          sendMsg.error = `server-side transport ${msg.transport} not found`
+          send(sendMsg, ws)
+        }else{
+          transport.connect({dtlsParameters: msg.dtlsParameters}).then(()=>{
+            send(sendMsg, ws)
+          })
+        }
+      } catch (e) {
+        console.error('error in /signaling/connect-transport', e);
+        sendMsg.error = `${e}`
+        send(sendMsg, ws)
+      }
+    })
+
+    handlers.set('produceTransport', (base) => {
+      const msg = base as MSProduceTransportMessage
+      const {rtpParameters, paused, ...msg_} = msg
+      const sendMsg:MSProduceTransportReply = msg_
       const transport = transports.get(msg.transport)
       if (!transport) {
-        console.error(`connect-transport: server-side transport ${msg.transport} not found`)
+        console.error(`produce-transport: server-side transport ${msg.transport} not found`)
         sendMsg.error = `server-side transport ${msg.transport} not found`
         send(sendMsg, ws)
       }else{
-        transport.connect({dtlsParameters: msg.dtlsParameters}).then(()=>{
-          send(sendMsg, ws)
+        transport.produce({
+          kind:msg.kind,
+          rtpParameters: msg.rtpParameters,
+          paused:msg.paused,
+          appData: { peer:msg.peer, transportId: transport.id}
+        }).then((producer)=>{
+          if(producer.type === 'simulcast'){
+            producer.close()
+            console.log(`Simulcast producer ${producer.id} created but closed`)
+          }else{
+            console.log(`${producer.type} producer ${producer.id} created`)
+            producer.on('transportclose', () => {
+              console.log('producer\'s transport closed', producer.id);
+              closeProducer(producer);
+            })
+            producers.set(producer.id, producer)
+            sendMsg.producer = producer.id
+            send(sendMsg, ws)
+            updateWorkerLoad()
+          }
         })
       }
-    } catch (e) {
-      console.error('error in /signaling/connect-transport', e);
-      sendMsg.error = `${e}`
-      send(sendMsg, ws)
-    }
-  })
-
-  handlers.set('produceTransport', (base) => {
-    const msg = base as MSProduceTransportMessage
-    const {rtpParameters, paused, ...msg_} = msg
-    const sendMsg:MSProduceTransportReply = msg_
-    const transport = transports.get(msg.transport)
-    if (!transport) {
-      console.error(`produce-transport: server-side transport ${msg.transport} not found`)
-      sendMsg.error = `server-side transport ${msg.transport} not found`
-      send(sendMsg, ws)
-    }else{
-      transport.produce({
-        kind:msg.kind,
-        rtpParameters: msg.rtpParameters,
-        paused:msg.paused,
-        appData: { peer:msg.peer, transportId: transport.id}
-      }).then((producer)=>{
-        if(producer.type === 'simulcast'){
-          producer.close()
-          console.log(`Simulcast producer ${producer.id} created but closed`)
-        }else{
-          console.log(`${producer.type} producer ${producer.id} created`)
-          producer.on('transportclose', () => {
-            console.log('producer\'s transport closed', producer.id);
-            closeProducer(producer);
-          })
-          producers.set(producer.id, producer)
-          sendMsg.producer = producer.id
-          send(sendMsg, ws)
-          updateWorkerLoad()
-        }
-      })
-    }
-  })
-  handlers.set('closeProducer', (base) => {
-    const msg = base as MSCloseProducerMessage
-    const producerObject = producers.get(msg.producer)
-    const {producer, ...msg_} = msg
-    const reply:MSCloseProducerReply = {
-      ...msg,
-    }
-    if (producerObject){
-      producers.delete(producer)
-      producerObject.close()
-      updateWorkerLoad()
-    }else{
-      reply.error = 'producer not found.'
-    }
-    send(reply, ws)
-  })
-
-  handlers.set('consumeTransport', (base) => {
-    const msg = base as MSConsumeTransportMessage
-    const {rtpCapabilities, ...msg_} = msg
-    const sendMsg:MSConsumeTransportReply = {...msg_}
-    const transport = transports.get(msg.transport)
-    if (!transport) {
-      console.error(`consume-transport: server-side transport ${msg.transport} not found`)
-      sendMsg.error = `server-side transport ${msg.transport} not found`
-      send(sendMsg, ws)
-    }else{
-      transport.consume({
-        producerId: msg.producer,
-        rtpCapabilities: msg.rtpCapabilities,
-        paused:true,
-        appData: { peer:msg.peer, transportId: transport.id}
-      }).then((consumer)=>{
-        consumers.set(consumer.id, consumer)
-        consumer.on('transportclose', () => {
-          log(`consumer's transport closed`, consumer.id)
-          closeConsumer(consumer)
-        })
-          consumer.on('producerclose', () => {
-            log(`consumer's producer closed`, consumer.id);
-            closeConsumer(consumer)
-        })
-        sendMsg.consumer = consumer.id
-        sendMsg.rtpParameters = consumer.rtpParameters
-        sendMsg.kind = consumer.kind
-        send(sendMsg, ws)
-      }).catch((e)=>{
-        console.error(`consume-transport: for producer ${msg.producer} failed`)
-        sendMsg.error = `consume for ${msg.producer} failed`
-        send(sendMsg, ws)
-      })
-    }
-  })
-
-  handlers.set('resumeConsumer', (base) => {
-    const msg = base as MSResumeConsumerMessage
-    const consumerObject = consumers.get(msg.consumer)
-    const {consumer, ...msg_} = msg
-    const reply:MSResumeConsumerReply = {
-      ...msg_,
-    }
-    if (consumerObject){
-      consumerObject.resume().then(()=>{
-        console.log(`consumer.resume() for ${consumer} succeed.`)
-        send(reply, ws)
-      }).catch(()=>{
-        reply.error = `consumer.resume() for ${consumer} failed.`
-        send(reply, ws)
-      })
-    }else{
-      reply.error = `consumer ${consumer} not found.`
+    })
+    handlers.set('closeProducer', (base) => {
+      const msg = base as MSCloseProducerMessage
+      const producerObject = producers.get(msg.producer)
+      const {producer, ...msg_} = msg
+      const reply:MSCloseProducerReply = {
+        ...msg,
+      }
+      if (producerObject){
+        producers.delete(producer)
+        producerObject.close()
+        updateWorkerLoad()
+      }else{
+        reply.error = 'producer not found.'
+      }
       send(reply, ws)
+    })
+
+    handlers.set('consumeTransport', (base) => {
+      const msg = base as MSConsumeTransportMessage
+      const {rtpCapabilities, ...msg_} = msg
+      const sendMsg:MSConsumeTransportReply = {...msg_}
+      const transport = transports.get(msg.transport)
+      if (!transport) {
+        console.error(`consume-transport: server-side transport ${msg.transport} not found`)
+        sendMsg.error = `server-side transport ${msg.transport} not found`
+        send(sendMsg, ws)
+      }else{
+        transport.consume({
+          producerId: msg.producer,
+          rtpCapabilities: msg.rtpCapabilities,
+          paused:true,
+          appData: { peer:msg.peer, transportId: transport.id}
+        }).then((consumer)=>{
+          consumers.set(consumer.id, consumer)
+          consumer.on('transportclose', () => {
+            log(`consumer's transport closed`, consumer.id)
+            closeConsumer(consumer)
+          })
+            consumer.on('producerclose', () => {
+              log(`consumer's producer closed`, consumer.id);
+              closeConsumer(consumer)
+          })
+          sendMsg.consumer = consumer.id
+          sendMsg.rtpParameters = consumer.rtpParameters
+          sendMsg.kind = consumer.kind
+          send(sendMsg, ws)
+        }).catch((e)=>{
+          console.error(`consume-transport: for producer ${msg.producer} failed`)
+          sendMsg.error = `consume for ${msg.producer} failed`
+          send(sendMsg, ws)
+        })
+      }
+    })
+
+    handlers.set('resumeConsumer', (base) => {
+      const msg = base as MSResumeConsumerMessage
+      const consumerObject = consumers.get(msg.consumer)
+      const {consumer, ...msg_} = msg
+      const reply:MSResumeConsumerReply = {
+        ...msg_,
+      }
+      if (consumerObject){
+        consumerObject.resume().then(()=>{
+          console.log(`consumer.resume() for ${consumer} succeed.`)
+          send(reply, ws)
+        }).catch(()=>{
+          reply.error = `consumer.resume() for ${consumer} failed.`
+          send(reply, ws)
+        })
+      }else{
+        reply.error = `consumer ${consumer} not found.`
+        send(reply, ws)
+      }
+    })
+
+    //  function defines which use worker etc.
+    function connectToMain(){
+      clearMediasoup()
+      ws = new websocket.WebSocket(config.mainServer)
+      ws.onopen = (ev) => {
+        let ip = hostinfo.ip
+        if (!ip) ip = 'localhost'
+        const msg:MSPeerMessage = {
+            type:'workerAdd',
+            peer:`${ip}_${worker.pid}`
+        }
+        console.log(`send ${JSON.stringify(msg)}`)
+        send(msg, ws)
+      }
+      ws.onmessage = (ev)=>{
+        const text = ev.data.toString()
+        //  console.log(text)
+        const base = JSON.parse(text) as MSMessage
+        console.log(`${base.type} received from ${(base as any).peer}.`)
+        const handler = handlers.get(base.type)
+        if (handler){
+            handler(base, ws)
+        }
+      }
+      ws.onerror = (ev)=>{
+        console.log(`ws error ${ev.message}, state:${ws.readyState}`)
+      }
     }
+
+    console.log('connecting to main server');
+    setInterval(()=>{
+      if (ws.readyState !== ws.OPEN){
+        console.log('Try to connect to main server.')
+        connectToMain()
+      }
+    }, 5000)
   })
-
-  //  function defines which use worker etc.
-  function connectToMain(){
-    clearMediasoup()
-    ws = new websocket.WebSocket(config.mainServer)
-    ws.onopen = (ev) => {
-      let ip = getIpAddress()
-      if (!ip) ip = 'localhost'
-      const msg:MSPeerMessage = {
-          type:'workerAdd',
-          peer:`${ip}_${worker.pid}`
-      }
-      console.log(`send ${JSON.stringify(msg)}`)
-      send(msg, ws)
-    }
-    ws.onmessage = (ev)=>{
-      const text = ev.data.toString()
-      //  console.log(text)
-      const base = JSON.parse(text) as MSMessage
-      console.log(`${base.type} received from ${(base as any).peer}.`)
-      const handler = handlers.get(base.type)
-      if (handler){
-          handler(base, ws)
-      }
-    }
-    ws.onerror = (ev)=>{
-      console.log(`ws error ${ev.message}, state:${ws.readyState}`)
-    }
-  }
-
-  console.log('connecting to main server');
-  setInterval(()=>{
-    if (ws.readyState !== ws.OPEN){
-      console.log('Try to connect to main server.')
-      connectToMain()
-    }
-  }, 5000)
-})
+}
